4f016bfa6a2dfd09b2e070dda64a585428eecb03
helloVietTran <trandanhviet192003@gmail.com>
2026-01-02 09:26:56 +0700

    Init code base

---
+++ backend/src/main/java/com/vietanh/readingstoryweb/ReadingstorywebApplication.java
@@ -0,0 +1,13 @@
+package com.vietanh.readingstoryweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingStoryWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingStoryWebApplication.class, args);
+	}
+
+}
